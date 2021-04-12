9fe5445c1e6272361bcb223d7e8ea0ae4355416d
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-04-12 11:12:08 +0300

    loose coupling

---
+++ ex1/softwareEx1/src/main/java/com/mycompany/softwareex1/looseCoupling/Personel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.softwareex1.looseCoupling;
+
+/**
+ *
+ * @author Enes Kızılcın <nazifenes.kizilcin@stu.fsm.edu.tr>
+ */
+public class Personel {
+    
+    
+    
+    void doTask()
+    {
+        ITask task = new Task();
+        task.doSomething();
+    }
+}
---
+++ ex1/softwareEx1/src/main/java/com/mycompany/softwareex1/looseCoupling/Test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.softwareex1.looseCoupling;
+
+/**
+ *
+ * @author Enes Kızılcın <nazifenes.kizilcin@stu.fsm.edu.tr>
+ */
+public class Test {
+ 
+    public static void main(String[] args) {
+        
+        Personel p = new Personel();
+        p.doTask();
+    }
+}
