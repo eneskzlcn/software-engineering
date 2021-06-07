0269b8f32849bf0f3638778dc2ad2c6f30ca5041
Enes Kızılcan <nazifenes.kizilcin@stu.fsm.edu.tr>
2021-06-07 04:39:54 +0300

    task pattern

---
+++ ex1/softwareEx1/src/main/java/com/mycompany/softwareex1/looseCoupling/CleaningTask.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.softwareex1.looseCoupling;
+
+/**
+ *
+ * @author Enes Kızılcın <nazifenes.kizilcin@stu.fsm.edu.tr>
+ */
+public class CleaningTask implements ITask{
+
+    @Override
+    public void doSomething() {
+        System.out.println("Do Clean");
+    }
+    @Override
+    public void notDoSomething() {
+    }
+    
+}
